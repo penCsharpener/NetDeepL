f3f1aa7ff40ab2a0cadc30b3b346421ebd446243
penCsharpener <26190934+penCsharpener@users.noreply.github.com>
2020-03-07 14:49:50 +0100

    add constants TranslationParameterNames

---
+++ src/NetDeepL/Models/TranslationParameterNames.cs
@@ -0,0 +1,16 @@
+﻿namespace NetDeepL.Models
+{
+    public static class TranslationParameterNames
+    {
+        public const string TEXT = "text";
+        public const string TARGET_LANG = "target_lang";
+        public const string SOURCE_LANG = "source_lang";
+        public const string SPLIT_SENTENCES = "split_sentences";
+        public const string PRESERVE_FORMATTING = "preserve_formatting";
+        public const string TAG_HANDLING = "tag_handling";
+        public const string NON_SPLITTING_TAGS = "non_splitting_tags";
+        public const string OUTLINE_DETECTION = "outline_detection";
+        public const string SPLITTING_TAGS = "splitting_tags";
+        public const string IGNORE_TAGS = "ignore_tags";
+    }
+}
